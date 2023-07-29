4ff821a513e148eba39a107eb510731635cc0d9f
k022g0027@g.neec.ac.jp <k022g0027@g.neec.ac.jp>
2023-07-29 21:16:14 +0900

    02_00終わり

--- main.cpp
+++ main.cpp
@@ -1,37 +1,45 @@
-#include <Windows.h>
-#include "WinApi.h"
-#include "DirectX.h"
+#include "Engine.h"
+#include "Triangle.h"
 
-const wchar_t kWindowTitle[] = { L"CG2_WinMain" };
+const wchar_t kWindowTitle[] = { L"CG2_サカモトイクミ" };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-#pragma region ウィンドウ
+	//初期化
+	WinApp* win_ = nullptr;
+	CreateEngine* Engine = new CreateEngine;
+	Engine->Initialization(win_, kWindowTitle, 1280, 720);
 
-    //WinApi::CreateWindowView(kWindowTitle);
-    DirectXCommon::DirectXInitialization();
+	Engine->VariableInialize();
+
+#pragma region ゲームループ
+
+	MSG msg{};
+
+	while (msg.message != WM_QUIT) {
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+		}
+		else {
+
+			//ゲームの処理
+			Engine->BeginFrame();
+			Engine->Update();
+
+			Engine->Draw();
+
+			Engine->EndFrame();
+
+		}
+	}
 
 #pragma endregion
 
+	OutputDebugStringA("Hello,DirectX!\n");
 
-
-#pragma region 
-
-    MSG msg{};
-
-    while(msg.message != WM_QUIT) {
-        
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-
-        } else {
-
-        }
-    }
-#pragma endregion
-
-    OutputDebugStringA("Hello,DirectX!\n");
-
-    return 0;
+	Engine->Finalize();
+	return 0;
 }
