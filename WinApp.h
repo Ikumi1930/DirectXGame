9b04dff94471e5b3762f0c2332de9c38b5edae0c
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2023-10-18 14:10:03 +0900

    aa

--- WinApp.h
+++ WinApp.h
@@ -2,14 +2,17 @@
 #include <Windows.h>
 #include <cstdint>
 #include <d3d12.h>
-#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib, "d3d12.lib")
 
 class WinApp {
 public:
+	// クライアント領域サイズ
+	static const int32_t kClientWidth = 1280;
+	static const int32_t kClientHeight = 720;
 
-	HINSTANCE GetHInstance()const { return wc_.hInstance; }
+	HINSTANCE GetHInstance() const { return wc_.hInstance; }
 
-	static	bool Procesmessage();
+	static bool Procesmessage();
 	static void Finalize();
 
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
@@ -21,13 +24,13 @@ public:
 	static void CreateWindowView(const wchar_t* title, int32_t clientWidth, int32_t clientheight);
 
 private:
-	static	UINT windowStyle_;
+	static UINT windowStyle_;
 
 	static ID3D12Debug1* debugController_;
 
-	static	inline 	RECT wrc_ = { 0,0,kClientWidth,kClientHeight };
+	static inline RECT wrc_ = {0, 0, kClientWidth, kClientHeight};
 
-	static inline	WNDCLASS wc_{};// ウィンドウクラス
+	static inline WNDCLASS wc_{}; // ウィンドウクラス
 
-	static	HWND hwnd_;
+	static HWND hwnd_;
 };
